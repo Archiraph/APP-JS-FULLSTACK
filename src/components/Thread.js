43051fe752fa7267de34d368aa5644e5fa5e50f3
Archiraph <raphael.mortiers@hotmail.com>
2024-01-17 23:32:47 +0100

    zdzd

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,3 +1,9 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Post from "./Post";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../feature/post.slice";
+
 const Thread = () => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts.postsData);

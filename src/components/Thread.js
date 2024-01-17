ce4bbca56891dca687c5ad7f21d27f666692eaa1
Archiraph <raphael.mortiers@hotmail.com>
2024-01-17 23:24:00 +0100

    zdzz

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -11,11 +11,26 @@ const Thread = () => {
   useEffect(() => {
     dispatch(getPosts());
   }, []);
+
   return (
     <div className="thread-container">
       {posts &&
         posts
-          .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
+          .filter((post) => {
+            if (
+              !post.createdAt ||
+              new Date(post.createdAt).toString() === "Invalid Date"
+            ) {
+              console.error("Invalid date:", post);
+              return false;
+            }
+            return true;
+          })
+          .slice()
+          .sort(
+            (a, b) =>
+              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          )
           .map((post) => <Post key={post._id} post={post} />)}
     </div>
   );

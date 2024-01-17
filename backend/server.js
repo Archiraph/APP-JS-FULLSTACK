3fe076ef6c0e986e3d5b460eef38e57abbe0ef7c
Archiraph <raphael.mortiers@hotmail.com>
2024-01-17 14:46:29 +0100

    5

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const port = 5000;
+
+// Connexion à la DB
+connectDB();
+
+const app = express();
+
+// Autorisation CORS
+app.use(
+  cors({
+    origin: "https://full-stack-app-zeta.vercel.app/",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+// Middleware qui permet de traiter les données du req (request)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/post", require("./routes/post.route"));
+
+// Lancer le serveur
+app.listen(port, () => console.log("Le serveur a démarré au port " + port));

6bc27f85baa5e3b04be8d0f1b997f053f8ed5d8a
Archiraph <raphael.mortiers@hotmail.com>
2024-01-17 23:49:36 +0100

    fefe

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../feature/user.slice";
 import postReducer from "../feature/post.slice";
+import thunk from "redux-thunk";
 
 export default configureStore({
   reducer: {
     user: userReducer,
     posts: postReducer,
   },
+  middleware: [thunk],
 });
